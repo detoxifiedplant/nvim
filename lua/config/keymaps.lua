b5ea09f11566f4abf2d51b732d89dd6e98e59f22
kamlesh <kamlesh@tc-pl.in>
2023-11-22 15:08:59 +0530

    comments
    comments mapped with leader

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local Util = require("lazyvim.util")
 local map = vim.keymap.set
 
 map("i", "jk", "<esc>l", { desc = "Escape Mode" })
@@ -16,8 +17,8 @@ map("v", "<S-q>", "<cmd>bdelete<cr>", { desc = "Delete Buffer" })
 -- map("x", "J", ":move '>+1<CR>gv-gv", { desc = "Move Lines" })
 -- map("x", "K", ":move '<-2<CR>gv-gv", { desc = "Move Lines" })
 
-map("n", "p", "p==", { desc = "Better Pasting" })
-map("n", "P", "P==", { desc = "Better Pasting" })
+-- map("n", "p", "p==", { desc = "Better Pasting" })
+-- map("n", "P", "P==", { desc = "Better Pasting" })
 map("v", "p", '"_dP', { desc = "Better Pasting" })
 map("v", "P", '"_dP', { desc = "Better Pasting" })
 
@@ -28,5 +29,12 @@ map("n", "<leader>si", ":norm =i{<CR>", { desc = "Indent Inside Paragraph" })
 map("n", "<leader>sf", ":norm V$%<CR>", { desc = "Select Entire Function" })
 map("n", "<A-q>", "<cmd>q!<CR>", { desc = "Quit Without Saving" })
 map("n", "<A-w>", "<cmd>w<CR>", { desc = "Save File" })
+
+map("n", "<leader>/", "gcc", { remap = true, silent = true, desc = "Comment line" })
+map("x", "<leader>/", "gc", { remap = true, silent = true, desc = "Comment selection" })
+
+map({ "n", "v" }, "<leader>sI", function()
+    Util.format({ force = true })
+end, { desc = "Format" })
 
 -- map("n", "<leader>e", "<cmd>Lex 30<CR>", { desc = "Save File" })
