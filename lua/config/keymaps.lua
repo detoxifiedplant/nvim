991afa074a0e2abdce72069b6b04fe814ddbd696
kamlesh <kamlesh@tc-pl.in>
2023-11-23 14:48:56 +0530

    key changes

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -22,7 +22,7 @@ map("v", "<S-q>", "<cmd>bdelete<cr>", { desc = "Delete Buffer" })
 map("v", "p", '"_dP', { desc = "Better Pasting" })
 map("v", "P", '"_dP', { desc = "Better Pasting" })
 
-map("n", "<leader>h", "<cmd>noh<cr>", { desc = "Remove Highlited Search" })
+-- map("n", "<leader>h", "<cmd>noh<cr>", { desc = "Remove Highlited Search" })
 
 map("n", "<leader>sp", ":norm vip<CR>", { desc = "Select Inside Paragraph" })
 map("n", "<leader>si", ":norm =i{<CR>", { desc = "Indent Inside Paragraph" })
@@ -30,8 +30,8 @@ map("n", "<leader>sf", ":norm V$%<CR>", { desc = "Select Entire Function" })
 map("n", "<A-q>", "<cmd>q!<CR>", { desc = "Quit Without Saving" })
 map("n", "<A-w>", "<cmd>w<CR>", { desc = "Save File" })
 
-map("n", "<leader>/", "gcc", { remap = true, silent = true, desc = "Comment line" })
-map("x", "<leader>/", "gc", { remap = true, silent = true, desc = "Comment selection" })
+map("n", "<leader>'", "gcc", { remap = true, silent = true, desc = "Comment line" })
+map("x", "<leader>'", "gc", { remap = true, silent = true, desc = "Comment selection" })
 
 map({ "n", "v" }, "<leader>sI", function()
     Util.format({ force = true })
