033f6a59c9557b20bb594db1e6e748d8c0db2938
kamal <kamlesh@tc-pl.in>
2023-11-29 19:06:05 +0530

    telescope chagnes

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,32 @@
+local M = {
+  "nvim-telescope/telescope.nvim",
+  event = "Bufenter",
+  cmd = { "Telescope" },
+  dependencies = {
+    {
+      "ahmedkhalf/project.nvim",
+    },
+  },
+}
+
+local actions = require "telescope.actions"
+
+M.opts = {
+  defaults = {
+    prompt_prefix = " ",
+    -- selection_caret = " ",
+    path_display = { "smart" },
+    -- file_ignore_patterns = { ".git/", "node_modules" },
+    mappings = {
+      i = {
+        ["<Down>"] = actions.move_selection_next,
+        ["<Up>"] = actions.move_selection_previous,
+        ["<A-j>"] = actions.move_selection_next,
+        ["<A-k>"] = actions.move_selection_previous,
+      },
+    },
+  },
+}
+
+return M
+
